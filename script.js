77df1aee281055a67ab7a562d8b8cb7627c52cd3
Widocf <martinloberia@gmail.com>
2023-04-09 15:49:30 -0300

    git commit "js y css de turnos"

---
+++ script.js
@@ -0,0 +1,49 @@
+//seleccionar elementos y almacenarlos en variables
+
+const formWizard = document.querySelector('#form-wizard');
+const nextButtons = document.querySelectorAll('.next-button');
+const prevButtons = document.querySelectorAll('.prev-button');
+
+let pasoActual = 0;
+
+//mostrar y ocultar los pasos
+
+function showStep(stepIndex) {
+const fieldsets = document.querySelectorAll('fieldset');
+fieldsets.forEach((fieldset, index) => {
+if (index === stepIndex) {
+fieldset.style.display = 'block';
+} else {
+fieldset.style.display = 'none';
+}
+});
+}
+
+// ojala muestre el resumen
+function actualizarConfirmacion() {
+document.querySelector('#show-nombres').textContent = document.querySelector('#nombres').value;
+document.querySelector('#show-apellidos').textContent = document.querySelector('#apellidos').value;
+document.querySelector('#show-email').textContent = document.querySelector('#email').value;
+document.querySelector('#show-telefono').textContent = document.querySelector('#telefono').value;
+document.querySelector('#show-odontologo').textContent = document.querySelector('#odontologo').value;
+document.querySelector('#show-date').textContent = document.querySelector('#date').value;
+}
+
+nextButtons.forEach((button) => {
+button.addEventListener('click', () => {
+    pasoActual++;
+showStep(pasoActual);
+if (pasoActual === 3) {
+    actualizarConfirmacion();
+}
+});
+});
+
+prevButtons.forEach((button) => {
+button.addEventListener('click', () => {
+pasoActual--;
+showStep(pasoActual);
+});
+});
+
+showStep(pasoActual);
